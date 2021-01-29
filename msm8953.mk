7dcde696e49a20f45c8595723a9dbac2c6979b57
laityts <laityts@gmail.com>
2021-01-30 05:21:05 +0800

    msm8953-common: fix fingerprint

--- msm8953.mk
+++ msm8953.mk
@@ -180,11 +180,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    libunwind
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
